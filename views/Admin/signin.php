6c9183fee3b38737d1ecee2464f4189f38a3972c
Ngo Duc Duy <duysadboy@gmail.com>
2025-11-09 21:43:24 +0700

    feat: integrate Lucide icons and enhance login form with improved error handling

--- views/Admin/signin.php
+++ views/Admin/signin.php
@@ -37,34 +37,85 @@
                 <img src="<?= BASE_URL ?>/assets/Img/logo_black.png" alt="adminVIVU Logo" class="max-w-50 w-full  mx-auto">
             </div>
 
-            <div class="w-full mb-6">
-                <div class="flex flex-row item-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative flex items-start space-x-3" role="alert">
-                    <i class='bx bx-error-circle text-2xl flex-shrink-0'></i>
-                    <div class="flex flex-row item-center">
-                        <strong class="font-bold">Lỗi!:</strong>
-                        <span class="block sm:inline">khôgn thành công </span>
+            <div id="errorlogin" class="" role="alert">
+                <?php if (isset($_SESSION['success'])): ?>
+                    <p class="font-medium text-sm bg-lime-100 border border-lime-400 text-lime-800 px-4 py-3 rounded relative mb-6">
+                        <?= htmlspecialchars($_SESSION['success']) ?>
+                    </p>
+                <?php endif ?>
+            </div>
+            <form id="formlogin" action="<?= BASE_URL ?>?mode=client&act=login" method="POST" class="space-y-6 w-full">
+
+                <div>
+                    <label for="login_email" class="block text-sm font-normal text-gray-700 mb-1">
+                        Email or username<span class="text-red-500">*</span>
+                    </label>
+                    <input
+                        type="text"
+                        id="login_email"
+                        name="username"
+                        required
+                        class="appearance-none relative block w-full px-3 py-2.5 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 **bg-blue-50/70** text-base transition duration-150" />
+                </div>
+
+                <div>
+                    <label for="login_password" class="block text-sm font-normal text-gray-700 mb-1">
+                        Password<span class="text-red-500">*</span>
+                    </label>
+                    <input
+                        type="password"
+                        id="login_password"
+                        name="password"
+                        required
+                        class="appearance-none relative block w-full px-3 py-2.5 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 **bg-blue-50/70** text-base transition duration-150" />
+                </div>
+
+                <div class="flex flex-row flex-wrap justify-between items-center 
+                [&>:first-child]:mr-[10px] [&>:first-child]:mb-[10px] sm:[&>:first-child]:mr-[0px]">
+                    <div class="flex items-center justify-between *:mr-2">
+                        <div class="flex items-center">
+                            <input
+                                type="checkbox"
+                                id="remember_me"
+                                name="remember_me"
+                                class="h-4 w-4 text-orange-500 focus:ring-orange-500 border-gray-300 rounded">
+                            <label for="remember_me" class="ml-2 block text-sm text-gray-900">
+                                Remember me
+                            </label>
+                        </div>
+                        <a href="<?= BASE_URL ?>?mode=client&act=resetPassword" class="text-sm font-[600] text-gray-600 hover:text-orange-500 hover:underline hover:bg-[#0000]">
+                            Forgot Password ?
+                        </a>
+                    </div>
+                    <div class="flex items-center">
+                        <input
+                            type="checkbox"
+                            id="remember_medky"
+                            name="remember_me"
+                            class="checkpasswrod h-4 w-4 text-orange-500 focus:ring-orange-500 border-gray-300 rounded">
+                        <label for="remember_me" class="ml-2 block text-sm text-gray-900">check password</label>
                     </div>
                 </div>
-            </div>
 
-            <form action="#" method="post" class="w-full *:mb-5
-            [&_input]:h-[70px] [&_button]:h-[70px]">
-                <div class="mb-6 md:mb-10">
-                    <label for="useremail" class="block text-gray-700 text-sm font-bold mb-2">* Email</label>
-                    <input type="email" name="useremail" placeholder="Email Đăng Nhập"
-                        class="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-blue-50 border-blue-200">
+                <div>
+                    <button
+                        type="submit"
+                        class="w-full flex justify-center py-2.5 px-4 border border-transparent text-base font-medium rounded-md text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 transition duration-150 ease-in-out uppercase"
+                        data-page="auth"
+                        data-action="login">
+                        LOG IN
+                    </button>
                 </div>
 
-                <div class="mb-4 md:mb-10">
-                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2">* Password</label>
-                    <input type="password" name="password" placeholder="Password Đăng Nhập"
-                        class="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-blue-50 border-blue-200">
+                <div class="mt-6 text-center text-sm text-gray-600">
+                    Don't have an account?
+                    <a href="<?= BASE_URL ?>?mode=client&act=showformsigin" class="text-gray-600 font-[800] hover:text-orange-500 hover:bg-[#0000]">
+                        Sign up
+                    </a>
                 </div>
 
-                <button type="submit"
-                    class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-4 rounded w-full transition duration-300">
-                    Signin
-                </button>
+                <div class="optionlogin">
+                </div>
             </form>
         </div>
     </section>
