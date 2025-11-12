81e401c051f816a83d6578d9ca9ed43bf54a87a1
Ngo Duc Duy <nducduyfpoly.vn@gmail.com>
2025-11-12 22:40:40 +0700

    xóa file không cần thiết

--- views/Admin/giotthieuAdmin.php
+++ views/Admin/giotthieuAdmin.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giới thiệu DuyDum StorMaty - Admin Tour Management</title>
-    <link rel="stylesheet" href="<?= CssAdmin ?>gioithieu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
