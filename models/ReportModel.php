f45cf19f80e49daa09aee0b41e6b40cfaa5497fc
Ngo Duc Duy <nducduyfpoly.vn@gmail.com>
2025-12-13 13:02:22 +0700

    fix dữ liệu cập nhật doaanh thu

--- models/ReportModel.php
+++ models/ReportModel.php
@@ -9,9 +9,10 @@ class ReportModel
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    
+
     public function getCompletedBookings() //lấy các tour đã hoàn thành
     {
+        $today = today();
         $sql = "
             SELECT 
                 b.id AS booking_id,
@@ -23,16 +24,18 @@ class ReportModel
                 b.customer_name
             FROM bookings b
             JOIN tours t ON b.tour_id = t.id
-            WHERE b.end_date < CURDATE()          
+            WHERE b.end_date < :today          
             ORDER BY b.end_date DESC
         ";
 
         $stmt = $this->conn->prepare($sql);
-        $stmt->execute();
+        $stmt->execute([
+            'today' => $today
+        ]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-   
+
     public function getTotalRevenueCompleted() //tính tổng doanh thu các tour có trạng thái thành công
     {
         $sql = "
@@ -45,7 +48,7 @@ class ReportModel
         return (int)$stmt->fetchColumn();
     }
 
-   
+
     public function countCompletedBookings() //tính số bk đã hoàn thành
     {
         $sql = "
